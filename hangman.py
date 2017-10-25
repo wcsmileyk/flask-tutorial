76f98e259aee73cb7b2bf5c58a38282902577e45
Wes Smiley <wesley.c.smiley@gmail.com>
2017-10-24 22:05:12 -0400

    Added pipenv support

--- hangman.py
+++ hangman.py
@@ -5,26 +5,26 @@ from gallows import gallow_pic
 def hangman(secret_word):
     '''
     secret_word: string, the secret word to guess.
-    
+
     Starts up an interactive game of Hangman.
-    
+
     * X At the start of the game, let the user know how many
       letters the secret_word contains and how many guesses s/he starts with.
-      
+
     * X The user should start with 6 guesses
 
     * Before each round, you should display to the user how many guesses
       s/he has left and the letters that the user has not yet guessed.
-    
+
     * Ask the user to supply one guess per round. Remember to make
       sure that the user puts in a letter!
-    
-    * The user should receive feedback immediately after each guess 
+
+    * The user should receive feedback immediately after each guess
       about whether their guess appears in the computer's word.
 
-    * After each guess, you should display to the user the 
+    * After each guess, you should display to the user the
       partially guessed word so far.
-    
+
     Follows the other limitations detailed in the problem write-up.
     '''
     letter_count = len(secret_word)
@@ -87,4 +87,3 @@ if __name__ == "__main__":
     wordlist = load_words('words.txt')
     secret_word = choose_word(wordlist)
     hangman(secret_word)
-
